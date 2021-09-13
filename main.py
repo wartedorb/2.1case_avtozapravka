4c6d90452d681a7c72e0599fe5586d39df450c41
Максим <maxiim114@gmail.com>
2021-09-13 19:17:26 +0500

    full stations

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ Developers:
 
 
 """
+
+
+with open('azs.txt', 'r', encoding='UTF-8') as F_in:
+    all_lines = F_in.readlines()
+
+fillings_max = dict()
+fillings_fuels = dict()
+for line in all_lines:
+    elements_in_line = line.split()
+    fillings_max[elements_in_line[0]] = elements_in_line[1]  # {'1': '3', '2': '2', '3': '4'}
+    fillings_fuels[elements_in_line[0]] = elements_in_line[2:]  # {'1': ['АИ-80'], '2': ['АИ-92'], '3': ['АИ-92', 'АИ-95', 'АИ-98']}
+
+
