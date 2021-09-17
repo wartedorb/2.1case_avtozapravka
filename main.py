5a4f83406a3cdf2d12665e47be5bb84e839e4073
Максим <maxiim114@gmail.com>
2021-09-17 22:00:00 +0500

    ya sam ne znay 4e nadelal

--- main.py
+++ main.py
@@ -9,18 +9,20 @@ Developers:
 
 """
 import random
-from datetime import time
+import datetime
 
 with open('azs.txt', 'r', encoding='UTF-8') as F_in:
     all_lines = F_in.readlines()
 
-fillings_max = dict()
+fillings_maxnfuels = dict()
 fillings_fuels = dict()
+tanks = []
 for line in all_lines:
     elements_in_line = line.split()
-    fillings_max[elements_in_line[0]] = elements_in_line[1]  # {'1': '3', '2': '2', '3': '4'}
-    fillings_fuels[elements_in_line[0]] = elements_in_line[2:]  # {'1': ['АИ-80'], '2': ['АИ-92'], '3': ['АИ-92', 'АИ-95', 'АИ-98']}
-
+    fillings_maxnfuels[elements_in_line[0]] = elements_in_line[1:]  # {'1': ['3', 'АИ-80'], '2': ['2', 'АИ-92'], '3': ['4', 'АИ-92', 'АИ-95', 'АИ-98']}
+    tanks.append(elements_in_line[0])
+print(tanks)
+q = dict().fromkeys(tanks)
 
 with open('input.txt', 'r', encoding='UTF-8') as F_in2:
     all_lines_2 = F_in2.readlines()
@@ -44,6 +46,34 @@ def time_to(x):
         return x2 + r
     else:
         return x2
+
+timings = dict()
+
+def dict_oftimings(element, filtime, result):
+    """Makes a dict {time_start: time_end}"""
+    hrs1 = int(element[:2])
+    mins1 = int(element[3:])
+    tm1 = datetime.timedelta(hours=hrs1, minutes=mins1)
+    tm2 = datetime.timedelta(hours=0, minutes=filtime)
+    res_time = tm1 + tm2
+    result[element] = str(res_time)
+
+
+
+
+
+def information(cars_info):
+    """Main"""
+    res = dict()
+    for el in cars_info:
+        whole = int(time_to(cars_info[el][0]))
+        dict_oftimings(el, whole, res)
+        print('В {} новый клиент: {} {} {} {} встал в очередь к автомату №{}'.format(el, '-', '-', '-', str(whole), '-'))
+    print(res)
+information(cars_info)
+
+
+
 
 
 
