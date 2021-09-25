65fd37c831bbde4e7636317274b1b3f60e2f9921
Kirill20704 <b.kirill.a.2001@gmail.com>
2021-09-25 19:58:05 +0700

    1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 """
 Case-study 'gas station'
-
 Developers:
         Kondrashov M. - 70%
         Bikmetov E. - 40%
@@ -9,7 +8,7 @@ Developers:
 
 from random import randint
 import datetime
-import datetime
+
 tanker_max = {}
 tanker_fuel = {}
 tankers_queue = {}
@@ -36,6 +35,7 @@ with open('input.txt', 'r') as F_in:
         car_liter[s[0]] = int(s[1])  # {'00:01': 10, '00:04': 45, '00:12': 40, '00:41': 30,
         car_type[s[0]] = s[2]  # {'00:01': 'АИ-80', '00:04': 'АИ-95', '00:12': 'АИ-92', '00:41': 'АИ-98',
 
+
 def time_to(x):
     """What time does it take to fuel up a car"""
     x = int(x)
@@ -49,12 +49,13 @@ def time_to(x):
     else:
         return x2
 
+
 car_time = {}
 for keys in car_liter:  # Получаем время на заправу
     car_time[keys] = time_to(car_liter[keys])  # {'00:01': 1, '00:04': 5, '00:12': 5, '00:41': 3, '00:54': 4, '01:05': 2,
 
+
 def end_time(time_1car):
-    """Getting a time of end"""
     result = {}
     for key in time_1car:
         tm1 = datetime.timedelta(hours=int(key[:2]), minutes=int(key[3:]))
@@ -62,10 +63,9 @@ def end_time(time_1car):
         result[key] = tm1 + tm2
     return result
 
+
 timings_end = end_time(car_time)  # {'00:01': '0:03:00', '00:04': '0:08:00', '00:12': '0:15:00', '00:41': '0:45:00',
 timings_start = {}
-for key in timings_end:
-    timings_start[timings_end[key]] = key
 
 
 def check_places(tankers, time_now):  # time_now looks like '00:30'
@@ -83,6 +83,7 @@ def check_places(tankers, time_now):  # time_now looks like '00:30'
         result[key] = l2
     return result
 
+
 def get_place(tankers, types_dict, car_fuel, max_t):
     """Gets a car queue"""
     qd = {}
@@ -90,8 +91,9 @@ def get_place(tankers, types_dict, car_fuel, max_t):
         if car_fuel in types_dict[key]:
             qd[key] = len(tankers[key])
     k = ''
+    dp = max(qd.values()) + 1
     for key in qd:
-        if qd[key] < max(qd.values()) + 1 and max_t[key] > qd[key]:
+        if qd[key] < dp and max_t[key] > qd[key]:
             dp = qd[key]
             k = key
     return [dp, k]
@@ -102,32 +104,41 @@ for key in timings_end:
     tankers_queue_before = tankers_queue.copy()
     tankers_queue = check_places(tankers_queue, key)
     for key2 in tankers_queue_before:
-        for el in tankers_queue_before[key2]:
-            if el not in tankers_queue[key2]:
-                tm = str(datetime.time(int(str(el).split(':')[0]), int(str(el).split(':')[1])))[:-3]
-                print('В  {}  клиент  {} {} {} {}  заправил свой автомобиль и покинул АЗС.'.format(tm, timings_start[el], car_type[timings_start[el]], car_liter[timings_start[el]], car_time[timings_start[el]] ))
-                tankers_queue_before[key2].remove(el)
-                [print('Автомат №'+key_2+'  максимальная очередь: '+str(tanker_max[key_2])+' Марки бензина: '+' '.join(tanker_fuel[key_2])+' ->'+len(tankers_queue_before[key_2])*'*') for key_2 in tanker_max]
+        g = set()
+        for i in range(len(tankers_queue_before[key2])):
+            if tankers_queue_before[key2][i] not in tankers_queue[key2]:
+                tm = str(datetime.time(int(str(tankers_queue_before[key2][i]).split(':')[0]), int(str(tankers_queue_before[key2][i])[:len(str(tankers_queue_before[key2][i])) - 3].split(':')[1])))[:-3]
+                print('В  {}  клиент  {} {} {} {}  заправил свой автомобиль и покинул АЗС.'.format(tm, timings_start[tankers_queue_before[key2][i]], car_type[timings_start[tankers_queue_before[key2][i]]], car_liter[timings_start[tankers_queue_before[key2][i]]], car_time[timings_start[tankers_queue_before[key2][i]]]))
+                g.add(tankers_queue_before[key2][i])
+                for key_2 in tanker_max:
+                    print('Автомат №' + key_2 + '  максимальная очередь: ' + str(tanker_max[key_2]) + ' Марки бензина: ' + ' '.join(tanker_fuel[key_2]) + ' ->' + len(set(tankers_queue_before[key_2])-g) * '*')
     l = get_place(tankers_queue, tanker_fuel, car_type[key], tanker_max)
     if l[1] != '':
-        print('В  {}  новый клиент:  {} {} {} {} встал в очередь к автомату {} '.format(key, key, car_type[key], car_liter[key], car_time[key], l[1]))
+        print('В  {}  новый клиент:  {} {} {} {} встал в очередь к автомату {} '.format(key, key, car_type[key],car_liter[key], car_time[key],l[1]))
         dict_types_liters[car_type[key]] += int(car_liter[key])
+        if l[0] != 0:
+            time_of = tankers_queue[l[1]][l[0]-1]+datetime.timedelta(hours=0, minutes=car_time[key])
+            timings_start[time_of] = key
+            tankers_queue[l[1]].append(time_of)
+        else:
+            time_of = datetime.timedelta(hours=int(key[:2]), minutes=int(key[3:])) + datetime.timedelta(hours=0,minutes=car_time[key])
+            timings_start[time_of] = key
+            tankers_queue[l[1]].append(time_of)
     else:
         not_served += 1
         print('В  {}  новый клиент:  {} {} {} {} не смог заправить автомобиль и покинул АЗС. '.format(key, key, car_type[key], car_liter[key], car_time[key]))
-    tankers_queue[l[1]].append(timings_end[key])
-    [print('Автомат №'+key3+'  максимальная очередь: '+str(tanker_max[key3])+' Марки бензина: '+' '.join(tanker_fuel[key3])+' ->'+len(tankers_queue[key3])*'*') for key3 in tanker_max]
+    [print('Автомат №' + key3 + '  максимальная очередь: ' + str(tanker_max[key3]) + ' Марки бензина: ' + ' '.join(tanker_fuel[key3]) + ' ->' + len(tankers_queue[key3]) * '*') for key3 in tanker_max]
 
 print('\n'+'_'*64)
 print('Количество литров, проданное за сутки по каждой марке бензина:')
 [print(key+': '+str(dict_types_liters[key])) for key in dict_types_liters]
 
 for key in dict_types_sum:
-    dict_types_sum[key] = round((dict_types_liters[key]*dict_types_cost[key]),2)
+    dict_types_sum[key] = dict_types_liters[key]*dict_types_cost[key]
 
 summ = 0
 for key in dict_types_sum:
     summ += dict_types_sum[key]
-print('\nОбщая сумма продаж за сутки: '+str(summ))
-print('\nКоличество клиентов, которые покинули АЗС не заправив автомобиль из-за «скопившейся» очереди.: '+str(not_served))
+print('\nОбщая сумма продаж за сутки: '+str(round(summ, 2)))
+print('\nКоличество клиентов, которые покинули АЗС не заправив автомобиль из-за «скопившейся» очереди: '+str(not_served))
 print('_'*64)
